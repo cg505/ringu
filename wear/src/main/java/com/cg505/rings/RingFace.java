377590e4ea208b3da5175443806db8758de79731
Christopher Cooper <christopher.c.cooper@gmail.com>
2016-12-16 14:03:33 -0500

    add initial project structure

---
+++ wear/src/main/java/com/cg505/rings/RingFace.java
@@ -0,0 +1,59 @@
+package com.cg505.rings;
+
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.support.wearable.watchface.CanvasWatchFaceService;
+import android.view.SurfaceHolder;
+
+/**
+ * Created by cooperc on 12/16/16.
+ */
+
+public class RingFace extends CanvasWatchFaceService {
+
+    @Override
+    public Engine onCreateEngine() {
+        /* provide your watch face implementation */
+        return new Engine();
+    }
+
+    /* implement service callback methods */
+    private class Engine extends CanvasWatchFaceService.Engine {
+
+        @Override
+        public void onCreate(SurfaceHolder holder) {
+            super.onCreate(holder);
+            /* initialize your watch face */
+        }
+
+        @Override
+        public void onPropertiesChanged(Bundle properties) {
+            super.onPropertiesChanged(properties);
+            /* get device features (burn-in, low-bit ambient) */
+        }
+
+        @Override
+        public void onTimeTick() {
+            super.onTimeTick();
+            /* the time changed */
+        }
+
+        @Override
+        public void onAmbientModeChanged(boolean inAmbientMode) {
+            super.onAmbientModeChanged(inAmbientMode);
+            /* the wearable switched between modes */
+        }
+
+        @Override
+        public void onDraw(Canvas canvas, Rect bounds) {
+            /* draw your watch face */
+        }
+
+        @Override
+        public void onVisibilityChanged(boolean visible) {
+            super.onVisibilityChanged(visible);
+            /* the watch face became visible or invisible */
+        }
+    }
+}
